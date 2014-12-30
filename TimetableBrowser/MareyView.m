cfa1c8ed2d35b6e92b4f776d8ce1d6abd5723152
Berend Schotanus <BS@BSchotanus.nl>
2014-12-30 09:45:51 +0100

    TimetableBrowser and rrrr_cocoa_wrapper are licensed under the Apache License, Version 2.0

--- TimetableBrowser/MareyView.m
+++ TimetableBrowser/MareyView.m
@@ -1,9 +1,21 @@
+//  Copyright (c) 2014 First Flamingo Enterprise B.V. / Bliksem Labs B.V.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 //  MareyView.m
 //  TimetableBrowser
 //
-//  Created by Stefan de Konnink on 20-12-14.
-//  Copyright (c) 2014 Bliksem Labs B.V. All rights reserved.
+//  Created by Stefan de Konink on 20-12-14.
 //
 
 #import "MareyView.h"
